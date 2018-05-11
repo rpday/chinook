6bb782871740db360adc0eee2f6e99f906fc23fc
Ryan P Day <ryanday7@gmail.com>
2018-05-11 03:41:14 -0700

    Add files via upload

--- FeSe.py
+++ FeSe.py
@@ -17,13 +17,13 @@ if __name__=="__main__":
     a,c =  3.7734,5.5258 
     avec = np.array([[a/np.sqrt(2),a/np.sqrt(2),0.0],[-a/np.sqrt(2),a/np.sqrt(2),0.0],[0.0,0.0,c]])
 
-    filenm = 'FeSe_RD.txt'
-    CUT,REN,OFF,TOL=a*3,1,0.15,0.001
-    G,X,M,Z,mM = np.array([0,0,0]),np.array([0,0.5,0]),np.array([0.5,0.5,0]),np.array([0,0,0.5]),np.array([-0.5,-0.5,0])
+    filenm = 'FeSe_o.txt'
+    CUT,REN,OFF,TOL=a*3,1,0.17,0.001
+    G,X,M,Z,mM = np.array([0,0,0]),np.array([0,0.5,0]),np.array([0.5,0.5,0]),np.array([0,0,0.5]),np.array([0.5,-0.5,0])
 
-	
+#    pt1,pt2,pt3 = np.zeros(3),np.array([0.2119,0.2119,0]),np.array([0.2119,-0.2119,0])
 
-    spin = {'soc':True,'lam':{0:0.04}}
+    spin = {'soc':True,'lam':{0:0.05}}
     
     slab_dict = {'bool':False,
                 'hkl':np.array([0,0,1]),
@@ -39,7 +39,7 @@ if __name__=="__main__":
             'slab':slab_dict}
 
     Kd = {'type':'F',
-			'pts':[-0.36*X,G,0.36*X],
+			'pts':[0.25*M,G,0.25*M],
 			'grain':400,
 			'labels':['X','$\Gamma$','X','X','$\Gamma$']}
 
@@ -57,37 +57,37 @@ if __name__=="__main__":
     Kobj = build_lib.gen_K(Kd,avec)
     TB = build_lib.gen_TB(Bd,Hd,Kobj)
     TB.solve_H()
-#    TB.plotting(-1.5,0.5)
-#    O = ops.LdotS(TB,axis='z',vlims=(-0.5,0.5),Elims=(-0.25,0.1))
+    TB.plotting(-0.25,0.1)
+    O = ops.LdotS(TB,axis='x',vlims=(-0.5,0.5),Elims=(-0.25,0.1))
 
 #    O = ops.fatbs(proj,TB,vlims=(0,1),Elims=(-1,1),degen=True)
     
 #    
 #    
-    ARPES_dict={'cube':{'X':[-0.2,0.2,10],'Y':[-0.2,0.2,10],'kz':0.0,'E':[-0.45,0.05,10]},
-                'SE':[0.005,0.01],
-                'directory':'C:\\Users\\rday\\Documents\\TB_ARPES\\2018\\TB_ARPES_2018\\FeSe',
-                'hv': 21.2,
-                'pol':np.array([0,np.sqrt(0.5),-np.sqrt(0.5)]),
-                'mfp':7.0,
-                'resolution':{'E':0.01,'k':0.02},
-                'T':[False,10.0],
-                'W':4.0,
-                'angle':np.pi/4,
-                'spin':None,
-                'slice':[False,-0.35]}
-                #'Brads':{'0-3-2-1':100.0,'0-3-2-3':0.0}}
-
+#    ARPES_dict={'cube':{'X':[-0.2,0.2,50],'Y':[-0.2,0.2,50],'kz':0.0,'E':[-0.45,0.05,100]},
+#                'SE':[0.005,0.01],
+#                'directory':'C:\\Users\\rday\\Documents\\TB_ARPES\\2018\\TB_ARPES_2018\\FeSe',
+#                'hv': 21.2,
+#                'pol':np.array([0,np.sqrt(0.5),-np.sqrt(0.5)]),
+#                'mfp':7.0,
+#                'resolution':{'E':0.01,'k':0.02},
+#                'T':[False,10.0],
+#                'W':4.0,
+#                'angle':np.pi/4,
+#                'spin':None,
+#                'slice':[False,-0.35]}
+#                #'Brads':{'0-3-2-1':100.0,'0-3-2-3':0.0}}
 #
-#    
-
-    expmt = ARPES.experiment(TB,ARPES_dict)
-    expmt.datacube(ARPES_dict)
-#    expmt.plot_slice(ARPES_dict)
-#    
-#    
-    expmt.plot_gui(ARPES_dict)
-
+##
+##    
+#
+#    expmt = ARPES.experiment(TB,ARPES_dict)
+#    expmt.datacube(ARPES_dict)
+###    expmt.plot_slice(ARPES_dict)
+###    
+###    
+#    expmt.plot_gui(ARPES_dict)
+#
 
     
     
--- LiFeAs.py
+++ LiFeAs.py
@@ -5,6 +5,7 @@ Created on Wed Feb 07 12:07:08 2018
 @author: rday
 """
 
+
     
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,13 +14,15 @@ import ARPES_lib as ARPES
 import scipy.ndimage as nd
 import operator_library as ops
 import plt_sph_harm as sph
+import matplotlib.cm as cm
 
 if __name__=="__main__":
+#def run_calc(filename):
     a,c =  3.7914,6.3639
     avec = np.array([[a/np.sqrt(2),a/np.sqrt(2),0.0],[-a/np.sqrt(2),a/np.sqrt(2),0.0],[0.0,0.0,c]])
 
-    filenm ="LiFeAs_RD.txt"
-    CUT,REN,OFF,TOL=a*3,1,0.061,0.001
+    filenm ="LiFeAs_test2.txt"
+    CUT,REN,OFF,TOL=a*3,1/2.17,0.030,0.001
     G,X,M,Z,A = np.array([0,0,0]),np.array([0,0.5,0]),np.array([0.5,0.5,0]),np.array([0,0,0.5]),np.array([0.5,0.5,0.5])
 
 	
@@ -39,11 +42,11 @@ if __name__=="__main__":
             'slab':slab_dict}
 
     K_dict = {'type':'F',
-			'pts':[M,G,X],
-			'grain':1000,
-			'labels':['$M$','$\Gamma$','$Z$']}
+			'pts':[0.3*X+0.1*Z,0.1*Z,0.3*X+0.1*Z],#
+			'grain':500,
+			'labels':['$0.3X$','$\Gamma$','$0.3X$','$\Gamma$','$Z$']}
     
-    spin_dict = {'soc':True,'lam':{0:0.04}}
+    spin_dict = {'soc':True,'lam':{0:0.04/2.17}}
 
     Ham_dict = {'type':'txt',
 			'filename':filenm,
@@ -52,6 +55,7 @@ if __name__=="__main__":
 			'offset':OFF,
 			'tol':TOL,
 			'so':spin_dict['soc']}
+    
         
 
  
@@ -59,39 +63,49 @@ if __name__=="__main__":
     basis_dict = build_lib.gen_basis(basis_dict,spin_dict)
     Kobj = build_lib.gen_K(K_dict,avec)
     TB = build_lib.gen_TB(basis_dict,Ham_dict,Kobj)
+    
+#    for ii in range(len(TB.mat_els)):
+#        if TB.mat_els[ii].i==TB.mat_els[ii].j:
+#            if ii<len(TB.basis)/2:
+#                TB.mat_els[ii].H.append([0,0,0,1.0e-9])
+#            else:
+#                TB.mat_els[ii].H.append([0,0,0,-1.0e-9])
+            
+    
+    
     TB.solve_H()
-#    TB.plotting(-0.3,.25)
-    O = ops.LdotS(TB,axis='z',vlims=(-0.5,0.5),Elims=(-0.4,0.25))
-    ktuple = ((-1.1718,1.1718,800),(-1.1718,1.1718,800),0.0)
-    Ef = -0.05
-    tol=0.001
-    FS = ops.FS(TB,ktuple,Ef,tol)
+    TB.plotting(-0.25,0.1)
+#    OS = ops.Sz(TB,vlims=(-0.5,0.5),Elims=(-0.25,0.1))
+#    OL = ops.Lz_path(TB,vlims=(-1,1),Elims=(-0.25,0.1))
+#    OLZ = ops.LzSz_path(TB,vlims=(-0.5,0.5),Elims=(-0.25,0.1))
+    O = ops.LdotS(TB,axis='z',vlims=(-0.5,0.5),Elims=(-0.25,0.05))
+###    OJ = ops.Jz_path(TB,vlims=(-0.5,0.5),Elims=(-0.25,0.1))
+#    ktuple = ((-0.4,0.4,800),(-0.4+1.17183176,0.4+1.17183176,800),0.0)
+#    Ef = -0.017
+#    tol=0.0008
+#    LDS = ops.LSmat(TB)
+#    FSLS = ops.O_surf(LDS,TB,ktuple,Ef,tol,vlims=(-0.1,0.1))
+#    FS = ops.FS(TB,ktuple,Ef,tol)
 #    d1d,d1u = {o.index:o.proj for o in TB.basis[:5]},{(o.index-5):o.proj for o in TB.basis[10:15]}
 #    d2d,d2u = {(o.index-5):o.proj for o in TB.basis[5:10]},{(o.index-10):o.proj for o in TB.basis[15:]}
 #    d1 = {**d1d,**d1u}
 #    d2 = {**d2d,**d2u}
 #    ind = 8
 #    psi_11 = np.array([list(TB.Evec[i,:5,ind])+list(TB.Evec[i,10:15,ind]) for i in range(len(Kobj.kpts))])
-#    psi_21 = np.array([list(TB.Evec[i,:5,ind+1])+list(TB.Evec[i,10:15,ind+1]) for i in range(len(Kobj.kpts))])
-#    psi_31 = np.array([list(TB.Evec[i,:5,ind+2])+list(TB.Evec[i,10:15,ind+2]) for i in range(len(Kobj.kpts))])
-#    psi_41 = np.array([list(TB.Evec[i,:5,ind+3])+list(TB.Evec[i,10:15,ind+3]) for i in range(len(Kobj.kpts))])
 #
 #    psi_12 = np.array([list(TB.Evec[i,5:10,ind])+list(TB.Evec[i,15:,ind]) for i in range(len(Kobj.kpts))])
-#    psi_22 = np.array([list(TB.Evec[i,5:10,ind+1])+list(TB.Evec[i,15:,ind+1]) for i in range(len(Kobj.kpts))])
-#    psi_32 = np.array([list(TB.Evec[i,5:10,ind+2])+list(TB.Evec[i,15:,ind+2]) for i in range(len(Kobj.kpts))])
-#    psi_42 = np.array([list(TB.Evec[i,5:10,ind+3])+list(TB.Evec[i,15:,ind+3]) for i in range(len(Kobj.kpts))])    
-#    
+#   
 #    psi_1 = np.array([list(TB.Evec[i,:,ind+2]) for i in range(len(Kobj.kpts))])
 #    df = {o.index:o.proj for o in TB.basis}
+#    psi_11 = np.array([list(TB.Evec[i,:5,ind:ind+2])+list(TB.Evec[i,10:15,ind:ind+2]) for i in range(len(Kobj.kpts))])
 #    
-##    
 #    for ki in range(len(Kobj.kpts)):
-#        psi = psi_1[ki,:]
-##        strnm = 'psi_plots\\so_GZ2_4_{:d}.png'.format(ki)
-#        sph.gen_psi(30,psi_31[ki],df)#,strnm)
-#        
-##    ki = 6
-#    psi = psi_21[ki,:]
+#        psi = psi_11[ki]
+#        strnm = 'psi_plots\\03_05\\so_GZ1_1_{:d}.png'.format(ki)
+#        sph.gen_psi(30,psi_11[ki],d1,strnm)
+#####        
+#####    ki = 6
+##    psi = psi_21[ki,:]
 ##    strnm = 'psi_plots\\no_so_GZ2_1_{:d}.png'.format(ki)
 #    sph.gen_psi(20,psi_11[ki],d1)
 #    sph.gen_psi(20,psi_21[ki],d1)
@@ -110,27 +124,74 @@ if __name__=="__main__":
 ##    
 ##    
 ##    
-    ARPES_dict={'cube':{'X':[-0.5,0.5,130],'Y':[-0.5,0.5,130],'kz':0.0,'E':[-0.6,0.1,400]},
-                'SE':[0.00,0.005],
-                'directory':'C:\\Users\\rday\\Documents\\TB_ARPES\\2018\\TB_ARPES_2018\\LiFeAs',
-                'hv': 26.0,
-                'pol':np.array([0,1,0]),
-                'mfp':7.0,
-                'resolution':{'E':0.03,'k':0.05},
-                'T':[True,20.0],
-                'W':4.0,
-                'angle':0.0,
-                'spin':None,
-                'slice':[False,-0.1]}
-
+#    ARPES_dict={'cube':{'X':[-0.4,0.4,200],'Y':[-0.4,0.4,200],'kz':0.1*np.pi/c,'E':[-0.45,0.3,400]},
+#                'SE':[0.005,0,0],
+#                'directory':'C:\\Users\\rday\\Documents\\TB_ARPES\\2018\\TB_ARPES_2018\\LiFeAs',
+#                'hv': 26.0,
+#                'pol':np.array([0,1,0]),
+#                'mfp':7.0,
+#                'resolution':{'E':0.07,'k':0.05},
+#                'T':[True,20.0],
+#                'W':4.0,
+#                'angle':np.pi/4,
+#                'spin':None,
+#                'slice':[False,-0.1]}
+#                #'mirror':np.array([0,1,1])}
+#
 ##
 ##    
 ##
 #    exp = ARPES.experiment(TB,ARPES_dict)
 #    exp.datacube(ARPES_dict)
-#    Ig=exp.plot_gui(ARPES_dict) #If you want to see another polarization, simply update ARPES_dict['pol'] and re-run exp.plot_slice(ARPES_dict)
+#    
+#    ARPES_dict['pol'] = np.array([0.5,-np.sqrt(0.5)*1.0j,np.sqrt(0.5)*1.0j])
+#    ARPES_dict['spin'] = [1,np.array([0,0,1])]
+#    CMU,CMUg = exp.spectral(ARPES_dict)
+#    ARPES_dict['spin'] = [-1,np.array([0,0,1])]
+#    CMD,CMDg = exp.spectral(ARPES_dict)
+#    ARPES_dict['pol'] = np.array([0.5,np.sqrt(0.5)*1.0j,-np.sqrt(0.5)*1.0j])
+#    ARPES_dict['spin'] = [1,np.array([0,0,1])]
+#    CPU,CPUg = exp.spectral(ARPES_dict)
+#    ARPES_dict['spin'] = [-1,np.array([0,0,1])]
+#    CPD,CPDg = exp.spectral(ARPES_dict)
+##    
+###    
+##    CPSg  = (np.sqrt(CPUg*CMDg)-np.sqrt(CPDg*CMUg))/(np.sqrt(CPUg*CMDg)+np.sqrt(CPDg*CMUg)+0.0001)
+##    CPS = (np.sqrt(CPU*CMD)-np.sqrt(CPD*CMU))/(np.sqrt(CPUg*CMDg)+np.sqrt(CPD*CMU)+0.0001)
+#    IP = np.sqrt((CPU+0.00001)*(CMD+0.00001))
+#    IA = np.sqrt((CPD+0.00001)*(CMU+0.00001))
+#    IPg = np.sqrt((CPUg+0.00001)*(CMDg+0.00001))
+#    IAg = np.sqrt((CPDg+0.00001)*(CMUg+0.00001))
+#    
+#    CPSg = (IPg-IAg)/(IPg+IAg)
+#    CPS = (IP-IA)/(IP+IA)
 #
-
-    
-    
-	#####
+#    x,e = np.linspace(*ARPES_dict['cube']['X']),np.linspace(*ARPES_dict['cube']['E'])
+#    X,E = np.meshgrid(x,e)
+#
+##    
+##    
+#
+##    
+##
+##    
+##
+###    Ig=exp.plot_gui(ARPES_dict) #If you want to see another polarization, simply update ARPES_dict['pol'] and re-run exp.plot_slice(ARPES_dict)
+##
+##
+#    fig = plt.figure()
+#    ax = fig.add_subplot(131)
+#    ax2 = fig.add_subplot(132)
+#    ax3 = fig.add_subplot(133)
+#    p1 = ax.pcolormesh(X,E,IP[100,:,:].T,cmap=cm.Greys_r)
+#    p2 = ax2.pcolormesh(X,E,IA[100,:,:].T,cmap=cm.Greys_r)
+#    p3 = ax3.pcolormesh(X,E,CPS[100,:,:].T,cmap=cm.RdBu,vmin= -0.3,vmax=0.3)
+#    plt.savefig('CPS_LiFeAs.pdf',transparent=True,rasterized=True)
+#
+###
+##    
+#    return TB,Kobj
+###	#####
+###    
+#if __name__=="__main__":
+#    TB,K=run_calc("LiFeAs_test2.txt")
