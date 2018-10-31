b19fd3b920b92955f7a2af85d4ad80a5fe921a84
Ryan P Day <ryanday7@gmail.com>
2018-10-30 17:18:21 -0700

    Add files via upload

---
+++ examples/PtCoO2.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 29 21:51:03 2018
+
+@author: rday
+"""
+
+#import numpy as np
+#import matplotlib.pyplot as plt
+#from mpl_toolkits.mplot3d import Axes3D
+import sys
+sys.path.append('C:/Users/rday/Documents/TB_ARPES/2018/TB_ARPES_2018/TB_ARPES-master/')
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import ubc_tbarpes.build_lib as build_lib
+import ubc_tbarpes.ARPES_lib as ARPES
+import scipy.ndimage as nd
+import ubc_tbarpes.operator_library as ops
+from scipy.optimize import curve_fit
+import ubc_tbarpes.rotation_lib as rotlib
+
+def trig_onsite(matels,Hco,Hto):
+    B = np.array([[1.0j,-np.sqrt(2)*1.0j,0,-np.sqrt(2),-1],[1.0j,-np.sqrt(2)*1.0j,0,np.sqrt(2),1.0],[0,0,np.sqrt(6),0,0],[np.sqrt(2)*1.0j,1.0j,0,1,-np.sqrt(2)],[np.sqrt(2)*1.0j,1.0j,0,-1.0,np.sqrt(2)]])*np.sqrt(1./6)
+    T = np.linalg.inv(B).T
+    Hc = np.identity(5)*np.array([Hco,Hco,Hto,0,0])
+    HcT = np.dot(np.linalg.inv(B),np.dot(Hc,B))
+    for ti in matels:
+        i,j = np.mod(ti.i,11),np.mod(ti.j,11)
+        if i<5 and j<5 and abs(ti.i-ti.j)<5:
+            ti.H.append([0,0,0,HcT[i,j]])
+    return matels
+    
+
+
+
+if __name__=="__main__":
+    a,c =  2.82,17.8 
+    avec = np.around(np.array([[0,a,0],[a*np.sqrt(3)/2,a/2,0],[0,0,c]]),5)
+    
+    RCo,RO1,RO2 = np.zeros(3),a*np.array([-1./np.sqrt(12),0.5,0.33]),a*np.array([np.sqrt(1./12),0.5,-0.33])
+    
+    ECo = -1.2
+    Hco,Hto = 1.0,0.5
+    Vpps = 0.5
+    Vppp=-0.3
+    SK = {'003322S':-0.3,
+          '003322P':0.15,
+          '003322D':0.0,
+          '013221S':-1.6,
+          '013221P':0.87,
+          '112211S':Vpps,
+          '112211P':Vppp,
+          '032xy':ECo,
+          '032yz':ECo,
+          '032ZR':ECo,
+          '032xz':ECo,
+          '032XY':ECo,
+          '121x':-4.0,
+          '121y':-4.0,
+          '121z':-3.2,#,
+          '221z':-7.0,
+          '221x':-6.0,
+          '221y':-6.0,
+          '122211S':Vpps,
+          '122211P':Vppp,
+          '222211S':Vpps,
+          '222211P':Vppp,
+          '023221P':0.87,
+          '023221S':-1.6}
+    
+    CUT,REN,OFF,TOL = 3.0,1.0,0.0,0.0001
+    
+    spin = {'bool':True,'soc':True,'lam':{0:0.07,1:0.0,2:0.0}}
+    
+    Bd = {'atoms':[0,1,2],
+			'Z':{0:27,1:8,2:8},
+			'orbs':[["32xy","32yz","32ZR","32xz","32XY"],["21y","21z","21x"],["21y","21z","21x"]],
+            'pos':[RCo,RO1,RO2],			
+            'spin':spin}
+   
+    G,K,M = np.array([0,0,0]),np.array([1./3,2./3,0]),np.array([0,0.5,0])
+
+    Kd = {'type':'F',
+          'avec':avec,
+			'pts':[G,M,K,G],
+			'grain':100,
+			'labels':['$\Gamma$','M','K','$\Gamma$']}
+
+
+    Hd = {'type':'SK',
+          'V':SK,
+          'avec':avec,
+			'cutoff':CUT,
+			'renorm':REN,
+			'offset':OFF,
+			'tol':TOL,
+			'spin':spin}
+    
+    slab_dict = {'avec':avec,
+      'miller':np.array([1,1,1]),
+      'thick':10, 
+      'vac':30,
+      'fine':(0,0),
+      'termination':(1,1)}
+ 
+
+    ARPES_dict={'cube':{'X':[-1.2,1.2,150],'Y':[-1.2,1.2,150],'kz':0.0,'E':[-0.5,0.05,150]},
+                'SE':[0.01,0.2],
+                'directory':'C:\\Users\\rday\\Documents\\TB_ARPES\\2018\\TB_ARPES_2018\\FeSe',
+                'hv': 110.0,
+                'pol':np.array([0,1,0]),
+                'mfp':7.0,
+                'slab':True,
+                'resolution':{'E':0.01,'k':0.05},
+                'T':[True,10.0],
+                'W':0.0,
+                'angle':np.pi/2,
+                'spin':None,
+                'slice':[False,0.0]}
+
+
+
+    	#####
+    Bd = build_lib.gen_basis(Bd)
+
+    Kobj = build_lib.gen_K(Kd)
+    TB = build_lib.gen_TB(Bd,Hd,Kobj)
+    TB.mat_els = trig_onsite(TB.mat_els,Hco,Hto)
+    TB.solve_H()
+    TB.plotting(-0.7,0.1)
+    
+    
+
+         
+    ARPES_expmt = ARPES.experiment(TB,ARPES_dict)
+    ARPES_expmt.plot_gui(ARPES_dict)
+
+    
+    
+    
---
+++ examples/copper_nbl.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 13 11:47:18 2018
+
+@author: rday
+"""
+import sys
+sys.path.append('C:/Users/rday/Documents/TB_ARPES/2018/TB_ARPES_2018/TB_ARPES-master/')
+
+import numpy as np
+import ubc_tbarpes.build_lib as build_lib
+import ubc_tbarpes.NBL_TB as CuTB
+import ubc_tbarpes.ARPES_lib as ARPES
+import ubc_tbarpes.operator_library as ops
+
+
+
+if __name__=="__main__":
+    a =  3.56
+
+    avec = np.around(2.51730014/2.51735729*np.array([[-1.2587, -0.7267,  2.0554],[ 1.2587, -0.7267,  2.0554],[-0.    ,  1.4534,  2.0554]]),4)
+    
+    CUT,REN,OFF,TOL=3.5,1,0,0.001
+    G,X,W,L,K = np.zeros(3),np.array([0,0.5,0.5]),np.array([0.25,0.75,0.5]),np.array([0.5,0.5,0.5]),np.array([0.375,0.75,0.375])
+	
+
+    fnm = 'cu_params_pan.txt'
+    tol = 0.0001
+    SK,CUT = CuTB.gen_Cu_SK(avec,fnm,tol)
+    OFF = CuTB.pair_pot(fnm,avec)/(-18)+1.55#-18#
+
+    spin = {'bool':False,'soc':True,'lam':{0:0.06}}
+
+    Bd = {'atoms':[0],
+			'Z':{0:29},
+			'orbs':[["40","31x","31y","31z","32xy","32xz","32yz","32ZR","32XY"]],
+			'pos':[np.zeros(3)],
+            'spin':spin}
+
+    Kd = {'type':'F',
+          'avec':avec,
+			'pts':[G,X,W,L,G,K],
+			'grain':100,
+			'labels':['$\Gamma$','X','W','L','$\Gamma$','K']}
+
+
+    Hd = {'type':'SK',
+			'V':SK,
+          'avec':avec,
+			'cutoff':CUT,
+			'renorm':REN,
+			'offset':OFF,
+			'tol':TOL,
+			'spin':spin}
+    
+    slab_dict = {'avec':avec,
+      'miller':np.array([1,1,1]),
+      'thick':18,
+      'vac':10,
+      'fine':(0,0),
+      'termination':(0,0)}
+    
+    Bd = build_lib.gen_basis(Bd)
+    Kobj = build_lib.gen_K(Kd)
+    TB = build_lib.gen_TB(Bd,Hd,Kobj,slab_dict)
+    
+
+    
+    
+#    G,M,K=np.zeros(3),np.array([0.5,0.5,0.0]),np.array([1./3,2./3,0.0])
+#    G,M,K = np.zeros(3),np.array([1.24795,0.72052,0.0]),np.array([0.83197,1.44103,0.0])
+#    
+#    Kd['type']='A'
+#    Kd['avec']=TB.avec
+#    Kd['pts'] = [G,M,K,G]
+#    TB.Kobj = build_lib.gen_K(Kd)
+#    Hd['avec'] = TB.avec
+
+    TB.solve_H()
+    TB.plotting()
+##    
+#    theta =np.linspace(0,2*np.pi,200)
+#    kvals = np.array([[0.1*np.cos(t),0.1*np.sin(t),0] for t in theta])
+# 
+#    
+#    
+#    TB.Kobj.kpts = kvals
+#    TB.Kobj.kcut = theta
+#    TB.Kobj.kcut_brk = [0,2*np.pi]
+#    TB.Kobj.labels=['0','2pi']
+#    TB.solve_H()
+###    TB.plotting(-1.5,1.5)
+####    
+#    Svx = ops.S_vec(len(TB.basis),np.array([1,0,0]))
+#    Svy = ops.S_vec(len(TB.basis),np.array([0,1,0]))
+#    Sproj = ops.surface_proj(TB.basis,10)
+#    Sxs = np.dot(Svx,Sproj)
+#    Sys = np.dot(Svy,Sproj)
+#    SX = ops.O_path(Sxs,TB,Kobj=TB.Kobj,vlims=(-0.25,0.25),Elims=(-1,-0.),degen=True)
+#    SY = ops.O_path(Sys,TB,Kobj=TB.Kobj,vlims=(-0.25,0.25),Elims=(-1,-0.),degen=True)
+#    
+    
+    ARPES_dict={'cube':{'X':[-1.45,1.45,100],'Y':[-1.45,1.45,100],'kz':0.0,'E':[-0.55,0.05,60]},
+        'SE':[0.04],
+        'directory':'/Users/ryanday/Documents/UBC/TB_ARPES-082018/examples/FeSe',
+        'hv': 21.2,
+        'pol':np.array([0,1,0]),
+        'mfp':5.0,
+        'slab':True,
+        'resolution':{'E':0.03,'k':0.1},
+        'T':[True,10.0],
+        'W':4.0,
+        'angle':np.pi/6,
+        'spin':None,
+        'slice':[False,-0.005]}
+    
+#    ARPES_expmt = ARPES.experiment(TB,ARPES_dict)
+#    ARPES_expmt.plot_gui(ARPES_dict)
