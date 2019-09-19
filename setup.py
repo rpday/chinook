7b732005b5a359b260424af0b6e4c4667413cdf5
rpday <rday@phas.ubc.ca>
2019-09-19 13:01:30 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -13,19 +13,23 @@ def readme():
     f.close()
     
 setup(name= 'chinook',
-      version = '2.0.0',
-      long_description=readme(),
-      classifiers =[
-              'Development Status ::4-Beta ',
-              'License :: MIT License',
-              'Programming Language :: Python :: 3 :: Only', 
-              'Topic :: Scientific/Engineering :: Physics ',
-              ],
+      version = '1.0.0',
       author = 'Ryan P. Day', 
-      author_email ='ryanday7@gmail.com',
+      author_email = 'ryanday7@gmail.com',
+      description = 'Tools for tight-binding and simulation of ARPES',
+      long_description=readme(),
+      url = 'https://github.com/rpday/chinook',
+      classifiers =[
+		'Development Status :: 4 - Beta',              
+		'License :: OSI Approved :: MIT License',              	
+		'Programming Language :: Python :: 3 :: Only',             
+        	'Topic :: Scientific/Engineering :: Physics',
+          'Operating System :: OS Independent'
+	      ],
+
       license= 'MIT',
       packages= ['chinook'],
-      install_requires=['numpy','matplotlib','scipy','Tkinter','psutil'],
+      install_requires=['numpy','matplotlib','scipy'],
       include_package_data=True,
       package_data = {'chinook':['atomic_mass.txt','electron_configs.txt']}
       )
