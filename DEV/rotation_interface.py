e8518222374b5bae3c2a351a00a16764e9efa2f3
Ryan P Day <ryanday7@gmail.com>
2019-11-09 21:10:28 -0800

    almost operational rotation_inteface
    still needs the settings panel to be included to change plot range. otherwise operating. could use some improved aesthetics still.

--- DEV/rotation_interface.py
+++ DEV/rotation_interface.py
@@ -80,6 +80,7 @@ class sample:
             - **photon_energy**: float, photon energy in eV
         
         '''
+        self.master = master
         self.lattice = master.lattice
         self.lightsource = master.lightsource
         self.experiment = master.experiment
@@ -98,7 +99,22 @@ class sample:
         self.draw_emission()
         
         
-    def define_kmesh(self,energy):#photon_energy,klimits):
+    def define_kmesh(self,binding_energy):
+        '''
+        Define k-vectors associated with the experimental region of interest.
+        If an ARPES experiment has been defined associated with the experiment,
+        takes the k-mesh from there. Otherwise, fills a unit square, with He1-alpha
+        photon energy
+        
+        
+        *args*:
+            
+            - **binding_energy**: float, binding energy of interest)
+    
+        *return*:
+            
+            - **Kpts**: numpy array of Nx4 float, kx,ky,kz,kn, momentum vectors
+        '''
         
         if self.experiment is None:
             klimits = [-1,1,-1,1]
@@ -110,7 +126,7 @@ class sample:
             Kn = np.sqrt(2*me/hb**2*(photon_energy)*q)*AA
         else:
             Kx,Ky = self.experiment.X,self.experiment.Y
-            Kn = np.sqrt(2*me/hb**2*(self.experiment.hv-self.experiment.W+energy)*q)*AA
+            Kn = np.sqrt(2*me/hb**2*(self.experiment.hv-self.experiment.W+binding_energy)*q)*AA
             
 
         Kpts = np.zeros((np.shape(Kx)[0]*np.shape(Kx)[1],4))
@@ -118,6 +134,7 @@ class sample:
         Kpts[:,1] = Ky.flatten()
         Kpts[:,2]  = Kn-np.linalg.norm(Kpts[:,:2],axis=1)
         Kpts[:,3] = np.linalg.norm(Kpts,axis=1)
+        
         return Kpts        
         
     def emission_angles(self):
@@ -142,6 +159,19 @@ class sample:
 
     def plottable_surface(self):
     
+        '''
+        Defines relevant vectors for drawing sample surface, as well as pertinent vectors
+        e.g. the Cartesian frame, cryostat rotation axes, photon incidence vector.
+        
+        *return*:
+            
+            -  **points**: numpy array of 4x3 float, coordinates of sample corners
+            
+            - **cart_lines**: numpy array of 3x2x3 float, endpoints of cartesian axes, with origin as common for all
+            
+            - **nvec_line**: numpy array of 2x3 float, photon incidence vector
+        
+        '''
         points = np.array([self.frame[0] + self.frame[1],self.frame[0]-self.frame[1],-self.frame[0]+self.frame[1],-self.frame[0]-self.frame[1]])
     
         cart_lines = np.array([[np.zeros(3),self.frame[0]],[np.zeros(3),self.frame[1]],[np.zeros(3),self.frame[2]]])
@@ -151,6 +181,11 @@ class sample:
         return points,cart_lines,cryo_axes,nvec_line
         
     def draw_frame(self):
+        '''
+        Execute rebuild of the sample plotting axes, filling in the sample surface
+        as well as all vectors of interest.
+        
+        '''
         points,ax_lines,cryo_axes,nvec_line = self.plottable_surface()
         
         self.axs[0].clear()
@@ -172,7 +207,14 @@ class sample:
         self.axs[0].set_aspect(1)
         self.figs[0].canvas.draw()
         
-    def draw_emission(self,energy=None):
+    def draw_emission(self):
+        
+        '''
+        Execute re-draw of ARPES intensity map. If experiment has not been
+        defined, just draws in a default intensity map to fill in as dummy-data.
+        
+        
+        '''
         
         
         self.theta_x,self.theta_y = self.emission_angles()
@@ -186,31 +228,46 @@ class sample:
             self.experiment.pol = self.lightsource.pol
             _,Imap = self.experiment.spectral()
             Xm,Ym = np.reshape(self.theta_x,np.shape(self.experiment.X)),np.reshape(self.theta_y,np.shape(self.experiment.Y))
-#            if energy is not None:
-            self.axs[1].pcolormesh(Xm,Ym,Imap[:,:,10],cmap=cm.Greys_r)
-        
-#        self.axs[1].scatter(self.theta_x,self.theta_y)
-        
-        self.axs[1].set_xlim(-60,60)
-        self.axs[1].set_ylim(-60,60)
+            self.axs[1].pcolormesh(Xm,Ym,Imap[:,:,self.master.plot_index],cmap=cm.Greys_r)
+                
+        self.axs[1].set_xlim(-25,25)
+        self.axs[1].set_ylim(-25,25)
         
         self.figs[1].canvas.draw()
         
         
     def build_experiment(self):
+        '''
+        Define the requisite vectors which establish the coordinate frame
+        of an experiment. This includes the 'lattice' or by default 
+        Cartesian basis vectors, as well as the axes of cryostat rotation,
+        and the lightsource incident vector. Note this frame gets redefined
+        every time the angles are updated so that the full rotation can be 
+        done (rather than applying an incremental rotation, which shouldn't
+        even really save much time computationally). Accordingly, 
+        the frame is defined in reference to the global experimental frame
+        
+        *return*:
+            
+            - **frame**: numpy array of 6x3 float, containing basis vectors [0,1,2], 
+            as well as tilt axis [3], polar axis [4] and the lightsource incidence [5].
+            
+            
+        
+        '''
     
         frame = np.zeros((6,3))
         frame[:3,:] = self.lattice
     
         frame[3,:] = np.array([1,0,0]) #tilt angle
         frame[4,:] = np.array([0,1,0]) #polar angle
-        frame[5,:] = self.lightsource.nvec
+        frame[5,:] = self.lightsource.nvec_global
     
         return frame
     
 
 
-    def do_rotate(self,rotation_matrix,frame,fixed=None):
+    def execute_rotation(self,cryo):
         
         '''
         Rotate sample around the normal azimuthal angle. This axis is fixed to the 
@@ -218,9 +275,9 @@ class sample:
         
         *args*:
             
-            - **angle**: float, angle in radians
+            - **rotation_matrix**: numpy array of 3x3 float, full rotation matrix
             
-            - **frame**: numpy array of 5x3 float, local sample and motor axes
+            - **cryo**: numpy array of 3x3 float, cryostat (tilt, polar) rotation
             
         *return*:
             
@@ -233,12 +290,10 @@ class sample:
         for azimuthal, (3,4) fixed
         
         '''
-        
-        rotated_frame = np.einsum('ij,kj->ki',rotation_matrix,frame)
-        if fixed is not None:
-            for fi in range(len(fixed)):
-                rotated_frame[fixed[fi]] = frame[fixed[fi]]
-
+        frame = self.build_experiment()
+        rotated_frame = np.einsum('ij,kj->ki',self.rotation_matrix,frame)
+        frame[4] = np.array([0,1,0])
+        frame[3] = np.dot(cryo,np.array([1,0,0]))
         return rotated_frame
     
     
@@ -263,34 +318,23 @@ class sample:
         
         '''
         
-        frame = self.build_experiment()
         angle_values = [ai*rad for ai in self.angles]
         euler = rotlib.Euler_to_R(*angle_values[:3])
         tilt = rotlib.Rodrigues_Rmat(np.array([1,0,0]),angle_values[4])
         polar = rotlib.Rodrigues_Rmat(np.array([0,1,0]),angle_values[3])
         
-        all_but_one = np.dot(polar,np.dot(tilt,euler))
-        az_prime = np.dot(np.dot(polar,tilt),np.array([0,0,1]))
-        tilt_ax = np.dot(np.dot(polar,tilt),np.array([1,0,0]))
+        cryo = np.dot(polar,tilt)
+        
+        az_prime = np.dot(cryo,np.array([0,0,1]))
         az_mat = rotlib.Rodrigues_Rmat(az_prime,angle_values[-1])
-        rotation_matrix = np.dot(az_mat,all_but_one)
         
-        frame = self.do_rotate(rotation_matrix,frame)
-        frame[4] = np.array([0,1,0])
-        frame[3] = tilt_ax
+        self.rotation_matrix = np.dot(az_mat,np.dot(cryo,euler))
         
-        
-        self.frame = frame
-        self.rotation_matrix = rotation_matrix
+        self.frame = self.execute_rotation(cryo)
         
         self.lightsource.update(self.rotation_matrix)
 
         
-        
-    
-        
-        
-
 class angle:
     '''
     Framework for widgets and control of sample orientation. Identical 
@@ -334,6 +378,10 @@ class angle:
         self.slider.grid(row=origin[0]+1,column=origin[1]+1)
     
     def update_angle_slide(self,event):
+        '''
+        Send updated slider value for parent angle to to execute reorientation
+        and drawing of plot windows
+        '''
 
         angle_value = self.slider.get()
         self.strvar.set('{:0.2f}'.format(angle_value))
@@ -344,8 +392,13 @@ class angle:
             self.sample.draw_emission()
     
     def update_angle_entry(self):
+        '''
+        Update entry in textbox for parent angle. Executes recalculation
+        of the sample orientation and drawing of plots.
+        '''
         angle_value = float(self.strvar.get())
         self.sample.angles[self.index] = angle_value
+        self.slider.set(angle_value)
         if self.sample is not None:
             self.sample.rotate_frame()
             self.sample.draw_frame()
@@ -353,6 +406,18 @@ class angle:
             
             
 class light:
+    '''
+    Light-vector object. Carries information on angle and orientation
+    of incidence of light, as well as polarization. 
+    
+    Independent incidence vectors are maintained with respect to 
+    sample and wrt laboratory frame: the light-vector controlled by user
+    is not going to be the same as the one seen by the user. We draw the lab
+    frame vector, but the sample reference frame vector is used for calculating
+    the photoemission spectrum.
+    
+    '''
+    
     
     def __init__(self):
         
@@ -366,6 +431,12 @@ class light:
         self.pol = np.array([0,1,0])
         
     def pol_update(self):
+        '''
+        Change the polarization type. Options are restricted
+        to linear vertical and horizontal, as well as circular plus,
+        circular minus, defined with respect to the incidence vector 
+        direction
+        '''
         if self.pol_type=='LH':
             self.pol = self.hpol
         elif self.pol_type == 'LV':
@@ -377,6 +448,19 @@ class light:
         
     
     def update(self,sample_rotation=None):
+        '''
+        Update the light definition. Redefine the basis polarization
+        vectors (LV and LH) as indicated by the angles theta, phi
+        defined as polar and azimuthal angles. If sample has also been
+        rotated, the polarization with respect to sample is rotated
+        further, by the inverse of the sample's rotation.
+        
+        *kwargs*:
+            
+            - **sample_rotation**: numpy array of 3x3 float, sample rotation
+            matrix
+        
+        '''
         
         rotmat = np.dot(rotlib.Rodrigues_Rmat(np.array([0,1,0]),self.theta*rad),rotlib.Rodrigues_Rmat(np.array([0,0,1]),self.phi*rad))
         
@@ -392,25 +476,59 @@ class light:
         
 class Application:
     
-    def __init__(self,lattice,experiment=None):
-        self.lattice = lattice
+    def __init__(self,experiment=None):
+        
+        self.lattice = np.identity(3)
+        self.plot_index = 0
         self.experiment = experiment
         self.lightsource = light()
         self.root = Tk.Tk()
         self.fig = []
         self.ax = []
         self.fig_win = []
+        if experiment is not None:
+            self.energy_domain = np.linspace(*self.experiment.cube[2])
+        else:
+            self.energy_domain = np.linspace(-1,1,20)
+        
 
         self.angle_values = np.zeros(6)
         self.angle_widgets = []
         
-        self.root.wm_title('CHINOOK ORIENTATION')
-        ### insert experiment object here
+        self._define_message()
         
+        self.root.wm_title('CHINOOK ORIENTATION')        
         
         self.make_panel()
         
+        
+    def _define_message(self):
+        
+        self.about_message ='This interactive program plots the experiment\n'
+        self.about_message+='orientation along with real-time calculations\n'
+        self.about_message+='of the photoemission spectra. The sample ori-\n'
+        self.about_message+='entation is defined by the Euler angles alpha\n'
+        self.about_message+='beta, gamma in the z-y-z convention. The\n'
+        self.about_message+='cryostat orientation is defined with a verti-\n'
+        self.about_message+='cal polar angle theta as well as tilt angle\n'
+        self.about_message+='phi (axis rotates with theta) and azimuthal\n'
+        self.about_message+='delta. The tilt and polar axes are drawn in\n'
+        self.about_message+='dashed black lines. The light incidence\n'
+        self.about_message+='vector is defined by a polar theta and azim-\n'
+        self.about_message+='uthal phi. This vector is drawn in violet.\n'
+        self.about_message+='The polarization can be chosen from linear\n'
+        self.about_message+='vertical and horizontal, as well as circular\n'
+        self.about_message+='plus and minus. Updates to text entries and\n'
+        self.about_message+='polarization are effective only by pressing\n'
+        self.about_message+='the *update* button. Plot axis ranges can be\n'
+        self.about_message+='adjusted under the *settings*.'
+        
     def _update(self):
+        '''
+        Update button forces execution of sample re-orientation
+        and calculation of spectra accordingly. 
+        
+        '''
         
         for ai in self.angle_widgets:
             ai.update_angle_entry()
@@ -419,16 +537,44 @@ class Application:
         self.sample.rotate_frame()
         self.sample.draw_frame()
         self.sample.draw_emission()
-            
         
-    
-    def generate_sample(self):
+    def _update_polarization(self):
+        self.lightsource.theta,self.lightsource.phi = self.nang[0].get(),self.nang[1].get()
+        self.lightsource.pol_type = self.pol_labels[self.polChoice.get()] 
+        self.lightsource.update()
+        
+                
+    def _update_energy(self,event):
+        binding_energy = self.energyslider.get()
+        self.plot_index = np.where(abs(self.energy_domain-binding_energy)==abs(self.energy_domain-binding_energy).min())[0][0]
+        self.sample.Kpts = self.sample.define_kmesh(binding_energy=binding_energy)
+        self.sample.draw_emission()
+        
+    def _update_settings(self):
+        print('update your settings')
+        
+    def _about_popup(self):
+        
+        top = Tk.Toplevel(self.root)
+        top.title('About Chinook Orientation')
+        
+        msg = Tk.Message(master=top, text=self.about_message)
+        msg.pack()
+        
+        closebutton = Tk.Button(master=top,text='Close',command=top.destroy)
+        closebutton.pack()
+
+#############################################################
+#####BUILD ALL REQUISITE WIDGETS AND ASSOCIATED OBJECTS######
+#############################################################        
+        
+    def _make_sample(self):
         crystal = sample(self)
         for ai in range(6):
             self.angle_widgets[ai].sample = crystal
         return crystal
         
-    def generate_angles(self):
+    def _make_angles(self):
         names = ['alpha','beta','gamma','theta','phi','delta']
         origins = [(5,0),(8,0),(11,0),(5,4),(8,4),(11,4)]
 
@@ -438,7 +584,7 @@ class Application:
             self.angle_widgets.append(angle(name=names[ii],index=ii,value=self.angle_values[ii],origin=origins[ii],master=self.root,valmin=angle_range[ii][0],valmax=angle_range[ii][1]))  
     
     def _make_quit(self,origin):
-        self.quit = Tk.Button(master=self.root,text="QUIT", command=self.root.quit)
+        self.quit = Tk.Button(master=self.root,text="QUIT", command=self.root.destroy)
         self.quit.grid(row=origin[0],column=origin[1])
         
     def _make_update(self,origin):
@@ -446,12 +592,7 @@ class Application:
         self.update = Tk.Button(master=self.root,text="UPDATE", command=self._update)
         self.update.grid(row=origin[0],column=origin[1])
         
-    def _update_polarization(self):
-        self.lightsource.theta,self.lightsource.phi = self.nang[0].get(),self.nang[1].get()
-        self.lightsource.pol_type = self.pol_labels[self.polChoice.get()] 
-        self.lightsource.update()
-        
-    def generate_nvec(self,origin):
+    def _make_nvec(self,origin):
         
         self.nang = [Tk.DoubleVar(),Tk.DoubleVar()]
         self.nangEntry = []
@@ -473,12 +614,9 @@ class Application:
             self.nangEntry[-1].grid(row=origin[0]+1,column=columns[ii])
             
         for ii in range(4):
-            self.polButtons.append(Tk.Radiobutton(master=self.root,text=self.pol_labels[ii],indicatoron=0,variable=self.polChoice,value=ii,command=self._update_polarization))
+            self.polButtons.append(Tk.Radiobutton(master=self.root,text=self.pol_labels[ii],relief='flat',indicatoron=0,variable=self.polChoice,value=ii,command=self._update_polarization))
             self.polButtons[-1].grid(row=origin[0],column=button_columns[ii])
-            
-    def update_energy(self):
-        print(self.energyslider.get())
-    
+
     
     def _make_figure(self,origin,projection=None):
         
@@ -489,29 +627,42 @@ class Application:
         self.ax.append(self.fig[-1].add_subplot(111,projection=projection))
         self.fig_win[-1].get_tk_widget().grid(row=origin[0],column=origin[1],columnspan=4,rowspan=4)
 
-    def _make_energy(self,origin,erange):
+    def _make_energy(self,origin):
         
         self.energylabel = Tk.Label(master=self.root,text='Binding Energy (eV)').grid(row=origin[0],column=origin[1])
-        self.energyslider = Tk.Scale(master=self.root,from_=erange[0],to=erange[-1],resolution=(erange[1]-erange[0]),orient='horizontal',relief='flat',sliderrelief='flat',command=self.update_energy)
+        self.energyslider = Tk.Scale(master=self.root,from_=self.energy_domain[0],to=self.energy_domain[-1],resolution=(self.energy_domain[1]-self.energy_domain[0]),orient='horizontal',relief='flat',sliderrelief='flat',command=self._update_energy)
         self.energyslider.grid(row=origin[0],column=origin[1]+1)
+        
+    def _make_settings(self,origin):
+        
+        self.settings_button = Tk.Button(master=self.root,text='Settings',command=self._update_settings)
+        self.settings_button.grid(row=origin[0],column=origin[1])
+        
+    def _make_about(self,origin):
+        
+        self.about_button = Tk.Button(master=self.root,text='About',command=self._about_popup)
+        self.about_button.grid(row=origin[0],column=origin[1])
+        
+        
+        
 
     def make_panel(self): 
         
         self._make_figure(origin=(0,0),projection="3d")
-        self._make_figure(origin=(0,4),projection=None)
-        self.generate_angles()
-        self.sample = self.generate_sample()
-        self.generate_nvec(origin=(14,0))
-        self._make_energy(origin=(17,0), erange=np.linspace(-1,1,100))
-        self._make_update(origin=(19,1))
-        self._make_quit(origin=(19,0))
+        self._make_figure(origin=(0,5),projection=None)
+        self._make_angles()
+        self.sample = self._make_sample()
+        self._make_nvec(origin=(14,0))
+        self._make_energy(origin=(17,0))
+        self._make_update(origin=(19,4))
+        self._make_quit(origin=(19,5))
+        self._make_settings(origin=(19,1))
+        self._make_about(origin=(19,0))
 
 
 if __name__ == "__main__":
-    lattice = np.identity(3)
-    photon_energy = 30
-    
-    app = Application(lattice=lattice,experiment=experiment)
+
+    app = Application()#experiment=expmt)
     app.root.mainloop()
 #root.destroy()
 
